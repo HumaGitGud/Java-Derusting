bb2f722bfe9283b8f6ed09a172d3a312e987fe23
HumaGitGud <khomidov.huma@student.greenriver.edu>
2025-01-06 19:36:50 -0800

    Completed the rest of the exercises

--- src/MapPractice.java
+++ src/MapPractice.java
@@ -1,28 +1,47 @@
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapPractice {
   public static void main(String[] args) {
     // Create a HashMap with String keys and Integer values and
     // assign it to a variable of type Map
+    HashMap<String, Integer> map = new HashMap<>();
 
     // Put 3 different key/value pairs in the Map
     // (it's OK to do this one-by-one)
+    map.put("Red", 0);
+    map.put("Green", 1);
+    map.put("Blue", 2);
 
     // Get the value associated with a given key in the Map
+    System.out.println(map.get("Green"));
 
     // Find the size (number of key/value pairs) of the Map
+    System.out.println(map.size());
 
-    // Replace the value associated with a given key (the size of the Map shoukld not change)
+    // Replace the value associated with a given key (the size of the Map should not change)
+    map.put("Red", -1);
 
     // Check whether the Map contains a given key
+    System.out.println(map.containsKey("Yellow"));
 
     // Check whether the Map contains a given value
+    System.out.println(map.containsValue(2));
 
     // Iterate over the keys of the Map, printing each key
+    for (String key : map.keySet()) {
+      System.out.println(key);
+    }
 
     // Iterate over the values of the map, printing each value
+    for (Integer value : map.values()) {
+      System.out.println(value);
+    }
 
     // Iterate over the entries in the map, printing each key and value
+    for (Map.Entry<String, Integer> entry : map.entrySet()) {
+      System.out.println("key: " + entry.getKey() + " - value: " + entry.getValue());
+    }
 
     /*
      * Usage tip!
--- src/SetPractice.java
+++ src/SetPractice.java
@@ -1,17 +1,29 @@
+import java.util.HashSet;
+
 public class SetPractice {
   public static void main(String[] args) {
     // Create a HashSet of Strings and assign it to a variable of type Set
+    HashSet<String> set = new HashSet<>();
 
     // Add 3 elements to the set
     // (It's OK to do it one-by-one)
+    set.add("Cat");
+    set.add("Dog");
+    set.add("Bird");
 
     // Check whether the Set contains a given String
+    System.out.println(set.contains("Cat"));
 
     // Remove an element from the Set
+    set.remove("Bird");
 
     // Get the size of the Set
+    System.out.println(set.size());
 
     // Iterate over the elements of the Set, printing each one on a separate line
+    for (String string : set) {
+      System.out.println(string);
+    }
 
     /*
      * Warning!
